6fac3b3af1ebcb31d69efd9ee2adbf45bc72e439
thomzera <93238590+thomzera@users.noreply.github.com>
2023-11-19 19:41:22 -0300

    refactor: fbv to cbv

--- project/accounts/urls.py
+++ project/accounts/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views as accounts_views
 
 urlpatterns = [
-    path('register/', view=accounts_views.register, name='register'),
-    path('login/', view=accounts_views.user_login, name='login'),
-    path('logout/', view=accounts_views.user_logout, name='logout'),
+    path('register/', view=accounts_views.UserRegister.as_view(), name='register'),
+    path('login/', view=accounts_views.UserLogin.as_view(), name='login'),
+    path('logout/', view=accounts_views.UserLogout.as_view(), name='logout'),
 ]
--- project/accounts/views.py
+++ project/accounts/views.py
@@ -1,43 +1,55 @@
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import AnonymousUser
 from django.shortcuts import redirect, render
+from django.views import View
 
 from .forms import LoginForm, RegistrationForm
 
 
-def register(request):
-    if request.method == 'POST':
+class UserLogin(View):
+    template_name = 'registration/login.html'
+
+    def get(self, request):
+        form = LoginForm()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = LoginForm(request, data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            password = form.cleaned_data.get('password')
+            user = authenticate(request, username=username, password=password)
+            
+            if user:
+                login(request, user)
+                return redirect('index')
+        return render(request, self.template_name, {'form': form})
+
+
+class UserRegister(View):
+    template_name = 'registration/register.html'
+
+    def get(self, request):
+        form = RegistrationForm()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
         form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
             password = form.cleaned_data.get('password1')
             user = authenticate(username=username, password=password)
-            
+
             if user and not isinstance(user, AnonymousUser):
                 login(request, user)
-            
-            return redirect('index') 
-    else:
-        form = RegistrationForm()
-    return render(request, 'registration/register.html', {'form': form})
+
+            return redirect('index')
+        return render(request, self.template_name, {'form': form})
 
 
-def user_login(request):
-    if request.method == 'POST':
-        form = LoginForm(request, data=request.POST)
-        if form.is_valid():
-            username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
-            user = authenticate(request, username=username, password=password)
-            if user:
-                login(request, user)
-                return redirect('index')
-    else:
-        form = LoginForm()
-    return render(request, 'registration/login.html', {'form': form})
-
-
-def user_logout(request):
-    logout(request)
-    return redirect('login')
+class UserLogout(View):
+    
+    def get(self, request):
+        logout(request)
+        return redirect('login')
