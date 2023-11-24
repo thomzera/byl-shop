9bf83b9d04e23769c2786c8431697827186ac1bf
thomzera <93238590+thomzera@users.noreply.github.com>
2023-11-24 09:15:59 -0300

    feat: login required mixin class param

--- project/core/views.py
+++ project/core/views.py
@@ -1,14 +1,15 @@
 # from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import DetailView, ListView
 
 from . import models as core_models
 
 
-class Index(ListView):
+class Index(LoginRequiredMixin, ListView):
     model = core_models.Produto
     template_name = 'core/pages/index.html'
 
 
-class ProductDetail(DetailView):
+class ProductDetail(LoginRequiredMixin, DetailView):
     model = core_models.Produto
     template_name = 'core/pages/detail.html'
