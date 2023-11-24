6490843fb282427eb659f7b9566a00db31ee8af0
thomzera <93238590+thomzera@users.noreply.github.com>
2023-11-24 09:17:24 -0300

    feat: url para concluir cadastro de usuario

--- project/accounts/urls.py
+++ project/accounts/urls.py
@@ -4,6 +4,7 @@ from . import views as accounts_views
 
 urlpatterns = [
     path('register/', view=accounts_views.UserRegister.as_view(), name='register'),
+    path('register/complete/<int:user_id>/', view=accounts_views.UserRegisterComplete.as_view(), name='register_complete'),
     path('login/', view=accounts_views.UserLogin.as_view(), name='login'),
     path('logout/', view=accounts_views.UserLogout.as_view(), name='logout'),
 ]
