a531ae0c4874760412b068115292efc4c402420b
thomzera <93238590+thomzera@users.noreply.github.com>
2023-11-28 23:49:13 -0300

    style: full page to refactor

--- project/core/urls.py
+++ project/core/urls.py
@@ -3,13 +3,7 @@ from django.urls import path
 from . import views as core_views
 
 urlpatterns = [
-    path("",
-         core_views.Index.as_view(),
-         name="index"
-         ),
-
-    path("produto/<slug:slug>",
-         view=core_views.ProductDetail.as_view(),
-         name="produto_detail"
-         )
+    path("", core_views.Index.as_view(), name="index"),
+    path("produto/<slug:slug>", view=core_views.ProductDetail.as_view(), name="produto_detail"),
+    path("template/", view=core_views.Template.as_view(), name="template")
 ]
--- project/core/views.py
+++ project/core/views.py
@@ -1,6 +1,6 @@
-# from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import DetailView, ListView
+from django.shortcuts import render
+from django.views.generic import DetailView, ListView, View
 
 from . import models as core_models
 
@@ -13,3 +13,10 @@ class Index(LoginRequiredMixin, ListView):
 class ProductDetail(LoginRequiredMixin, DetailView):
     model = core_models.Produto
     template_name = 'core/pages/detail.html'
+
+
+class Template(View):
+    template_name = 'core/pages/template.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
